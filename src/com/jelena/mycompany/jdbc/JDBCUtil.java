7c369db8d0cc7424b408bde5625550151b34aec8
Jelena Gavanski <jelenagavanski@gmail.com>
2017-05-07 18:48:32 +0200

    Adds JDBC utility class.

---
+++ src/com/jelena/mycompany/jdbc/JDBCUtil.java
@@ -0,0 +1,86 @@
+package com.jelena.mycompany.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCUtil {
+	
+	public static Connection getConnection() throws SQLException {	
+			
+		//	moj drajver za MySql bazu registrujem, loading the driver class into the JVM,
+		//	driver class: com.mysql.jdbc.Driver
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException e) {			
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {			
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {			
+			e.printStackTrace();
+		}
+				
+		// Construct the connection URL	
+		String url ="jdbc:mysql://localhost:3306/mycompany";
+		String user="root";
+		String password = "";		
+		
+		// Get a connection
+		Connection conn = DriverManager.getConnection(url, user, password);
+		// Set the auto-commit off
+		conn.setAutoCommit(false);
+		return conn;
+	}
+
+	public static void closeConnection(Connection conn) {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void closeStatement(Statement stmt) {
+		try {
+			if (stmt != null) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void commit(Connection conn) {
+		try {
+			if (conn != null) {
+				conn.commit();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void rollback(Connection conn) {
+		try {
+			if (conn != null) {
+				conn.rollback();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
