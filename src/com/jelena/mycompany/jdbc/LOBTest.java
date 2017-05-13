ca00de79701e1af965c48734484ce0324d04e889
Jelena Gavanski <jelenagavanski@gmail.com>
2017-05-14 00:00:53 +0200

    Updates CLOB data. Retrieves data from CLOB.

--- src/com/jelena/mycompany/jdbc/LOBTest.java
+++ src/com/jelena/mycompany/jdbc/LOBTest.java
@@ -3,13 +3,18 @@ package com.jelena.mycompany.jdbc;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Blob;
+import java.sql.Clob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -29,7 +34,9 @@ public class LOBTest {
 		String inPicturePath = "picture7.jpg";
 		ensureFileExistence(inPicturePath);
 		try {
-			conn = JDBCUtil.getConnection();		
+			conn = JDBCUtil.getConnection();
+			
+			// UPDATE PICTURE
 			try {				
 				pstmt = getUpdateSQL(conn);			
 				//updateEmployeePicture(conn, pstmt, 3, inPicturePath);				
@@ -45,7 +52,7 @@ public class LOBTest {
 				JDBCUtil.rollback(conn);
 			}
 			
-			// CITANJE	
+			// RETRIEVE PICTURE	
 			//String outPicturePath = "out_picture6.jpg";	
 			String outPicturePath = "out_picture7.jpg";			
 			try {
@@ -58,7 +65,37 @@ public class LOBTest {
 				System.out.println("Retrieving employee's picture failed: ");
 				System.out.println(e.getMessage());
 				JDBCUtil.rollback(conn);
-			}				
+			}
+			
+			// UPDATE CV
+			String inCvPath = "cv7.txt";
+			ensureFileExistence(inCvPath);
+			try {				
+				pstmt = getUpdateCvSQL(conn);				
+				updateEmployeeCv(conn, pstmt, 7, inCvPath);
+				JDBCUtil.commit(conn);
+				System.out.println("Updated employee's cv successfully.");					
+			}
+			catch(SQLException e) {		
+				System.out.println("Updating employee's cv failed: ");
+				System.out.println(e.getMessage());
+				JDBCUtil.rollback(conn);
+			}
+			
+			// RETRIEVE CV
+			String outCvPath = "out_cv7.txt";
+			try {				
+				retrieveEmployeeCv(conn, 7, outCvPath);
+				//retrieveEmployeeCv(conn, 8, outCvPath);
+				JDBCUtil.commit(conn);
+				System.out.println("Retrieved and saved employee's cv successfully");				
+			}
+			catch(SQLException e) {
+				System.out.println("Retrieving employee's cv failed: ");
+				System.out.println(e.getMessage());
+				JDBCUtil.rollback(conn);
+			}
+				
 		}
 		catch(Exception e) {
 			System.out.println(e.getMessage());
@@ -122,6 +159,14 @@ public class LOBTest {
 		return pstmt;
 	}
 	
+	public static PreparedStatement getUpdateCvSQL(Connection conn) throws SQLException {
+		String SQL = "UPDATE employees " +
+				"SET cv = ? " +
+				"WHERE employee_id = ? ";				
+		PreparedStatement pstmt = conn.prepareStatement(SQL);
+		return pstmt;
+	}	
+	
 	
 	public static void ensureFileExistence(String filePath) {
 		Path path = Paths.get(filePath);
@@ -169,5 +214,97 @@ public class LOBTest {
 			fos.write(b);
 		}
 		fos.close();		
+	}
+	
+	
+	public static void updateEmployeeCv(Connection conn,
+			PreparedStatement pstmt,
+			int employeeId,
+			String cvFilePath) throws SQLException {
+		try {			
+			if (cvFilePath != null) {
+				// We need to create a Clob object first
+				Clob cvClob = conn.createClob();
+				readInCvData(cvClob, cvFilePath);
+				pstmt.setClob(1, cvClob);
+			}
+			else {
+				pstmt.setNull(1, Types.CLOB);
+			}
+			pstmt.setInt(2, employeeId);		
+			pstmt.executeUpdate();					
+		}
+		catch (IOException | SQLException e) {
+			throw new SQLException(e);
+		}
+		finally {
+			JDBCUtil.closeStatement(pstmt);
+		}		
+	}
+	
+	// iz txt fajla u clob
+	public static void readInCvData(Clob cvClob, String cvFilePath)
+			throws FileNotFoundException, IOException, SQLException {
+			// Get the character output stream of the Clob object
+			// to write the resume data to it.
+			int startPosition = 1; // start writing from the beginning
+			Writer writer = cvClob.setCharacterStream(startPosition); // za upis u clob
+			FileReader fr = new FileReader(cvFilePath); // za citanje txt fajla
+			// Read from the file and write to the Clob object
+			int b = -1;
+			while ((b = fr.read()) != -1) {
+				writer.write(b);
+			}
+			fr.close();
+			writer.close();
 	}	
+	
+
+	public static void retrieveEmployeeCv(Connection conn,
+			int employeeId, 
+			String cvPath) 
+					throws SQLException {
+		String SQL = "SELECT employee_id, cv " +
+					"FROM employees " +
+					"where employee_id = ?";
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, employeeId);
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {							
+				Clob cvClob = rs.getClob("cv");
+				if (cvClob != null) {
+					saveCv(cvClob, cvPath);
+					cvClob.free();
+				}	
+				// What should I do if the cvClob is null?
+				// perhaps it would be better if function returned false
+				// in the case there is no cv
+				// true otherwise
+				//else {
+					//System.out.println("There is no cv");
+				//}
+			}
+		}
+		catch(IOException | SQLException e) {
+			throw new SQLException(e);			
+		}
+		finally {
+			JDBCUtil.closeStatement(pstmt);
+		}
+	}
+	
+	
+	public static void saveCv(Clob resumeClob, String filePath)
+			throws SQLException, IOException {
+			FileWriter fw = new FileWriter(filePath); // za upis u txt fajl
+			Reader reader = resumeClob.getCharacterStream(); // za citanje clob-a
+			int b = -1;
+			while ((b = reader.read()) != -1) {
+				fw.write((char) b);
+			}	
+			fw.close();
+	}		
 }
